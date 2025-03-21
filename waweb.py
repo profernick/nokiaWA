f4a853c9fad8132c843d1e4cb9583ea1f536ec01
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2025-03-21 15:48:30 +0700

    feat: attempt to display list of chats with latest messages (still buggy)

--- waweb.py
+++ waweb.py
@@ -50,5 +50,16 @@ def hello_world():
 @app.route("/logged-in")
 def logged_in():
     if session.get("logged_in"):
+        # continue this, instead of only displaying ur in, start displaying chats!
         return "<p>Ur in...</p>"
     return "<p>U aint in bro...</p>"
+
+@app.route("/chats")
+def chats():
+    load_chats = driver.execute_script("window.Store = Object.assign({}, window.require('WAWebCollections'));")
+
+    contacts = driver.execute_script("return window.Store.Chat.map(contacts => contacts.formattedTitle);")
+
+    latest_msg = driver.execute_script("return window.Store.Chat._models.flatMap(chatd => window.Store.Chat.get(chatd.id._serialized).msgs._models.slice(-1).map(msg => msg.body));")
+
+    return render_template("chats.html", contacts=contacts, latest=latest_msg)
